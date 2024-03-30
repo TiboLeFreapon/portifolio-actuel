22d53052763efbf9220631df3a05d10b7eeaf752
thibDev <schmitt.thibault34070@gmail.com>
2024-03-30 13:59:44 -0400

    Modification de l'acceuil.

--- src/components/PanneauAcceuil.js
+++ src/components/PanneauAcceuil.js
@@ -1,13 +1,21 @@
 const PanneauAccueil = () => {
 
+    const logoReact = require("../img/acc/react.png");
+    const logoweb = require("../img/acc/web.png");
     return (
         <div className="panneau-acc">
+
+                <div className="fond-ecran">
+                    <img src={require("../img/acc/fondreact.png")}/>
+                </div>
             <div className="contenu-panneau-acc">
                 <h1 className="titre">
-                    Thibault Schmitt
+                    <span className="prenom">Thibault</span>
+                    <span className="nom">Schmitt</span>
                 </h1>
                 <h3 className="sous-titre">
-                    Développeur web
+                   <span className="dev">Développeur</span> 
+                   <span className="web">web</span>
                 </h3>
                 <div className="pres-site">
                     <span>Bienvenue sur mon site web ! J'espère que l'expérience vous plaira.</span><br/>
@@ -16,6 +24,5 @@ const PanneauAccueil = () => {
         </div>
     )
 }
-
 
 export default PanneauAccueil;

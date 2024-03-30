26f5acd01dc3e2f7f4d1940a33123ec5ee2f8f32
thibDev <schmitt.thibault34070@gmail.com>
2024-03-30 17:01:32 -0400

    Changement couleur projet. Ajout de variable Sass.

--- src/components/Projet.js
+++ src/components/Projet.js
@@ -1,14 +1,15 @@
 const Projet = ({ nom, description, projetPro, imgFond, lien }) => {
 
     //backgroundImage: `url("../img/github.png")`
+
+    {/* <a href={lien} target="_blank" rel="noopener noreferrer" style={{backgroundImage: `url(${imgFond})`}}> */ }
     return (
-        <a href={lien} target="_blank" rel="noopener noreferrer" style={{backgroundImage: `url(${imgFond})`}}>
+        <a href={lien} target="_blank" rel="noopener noreferrer">
             <div className={"projet " + nom} >
                 <div className="description">
-
-                <span className="projet-pp">{projetPro ? "Projet professionnel" : "Projet personnel"}</span>
-                <span className="titre">{nom}</span>
-                <span className="description-proj">{description}</span>
+                    <span className="projet-pp">{projetPro ? "Projet professionnel" : "Projet personnel"}</span>
+                    <span className="titre">{nom}</span>
+                    <span className="description-proj">{description}</span>
                 </div>
             </div>
         </a>

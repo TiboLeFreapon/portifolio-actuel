550461b3122375b64d05aed67261f1c44105f83f
thibDev <schmitt.thibault34070@gmail.com>
2024-03-31 17:48:32 -0400

    Creation animation fond

--- src/components/PanneauAcceuil.js
+++ src/components/PanneauAcceuil.js
@@ -1,7 +1,47 @@
+import { useState } from "react";
+import { useEffect } from "react";
+
 const PanneauAccueil = () => {
 
+   const [textAffichayA, settextAffichayA] = useState("");
+   const [textAffichayB, settextAffichayB] = useState("");
+   const [textAffichayC, settextAffichayC] = useState("");
+   const [textAffichayD, settextAffichayD] = useState("");
+   const [textAffichayE, settextAffichayE] = useState("");
     const logoReact = require("../img/acc/react.png");
     const logoweb = require("../img/acc/web.png");
+
+    const texteA = "Thibault";
+    const texteB = "Schmitt";
+    const texteC = "Développeur";
+    const texteD = "web";
+    const texteE = "Bienvenue sur mon site web ! J'espère que l'expérience vous plaira."
+    const texteF = "Il est tout nouveau, je travaille dessus en ce moment."
+
+    useEffect(()=>{
+        if(textAffichayA.length < texteA.length){
+            setTimeout(()=>{
+                settextAffichayA(textAffichayA + texteA[textAffichayA.length]);
+            }, 150)
+        } else if(textAffichayB.length < texteB.length){
+            setTimeout(()=>{
+                settextAffichayB(textAffichayB + texteB[textAffichayB.length]);
+            }, 150)
+        } else if(textAffichayC.length < texteC.length){
+            setTimeout(()=>{
+                settextAffichayC(textAffichayC + texteC[textAffichayC.length]);
+            }, 150)
+        }else if(textAffichayD.length < texteD.length){
+            setTimeout(()=>{
+                settextAffichayD(textAffichayD + texteD[textAffichayD.length]);
+            }, 150)
+        }else if(textAffichayE.length < texteE.length){
+            setTimeout(()=>{
+                settextAffichayE(textAffichayE + texteE[textAffichayE.length]);
+            }, 70)
+        }
+    }, [textAffichayA, textAffichayB, textAffichayC, textAffichayD, textAffichayE])
+
     return (
         <div className="panneau-acc">
 
@@ -10,16 +50,16 @@ const PanneauAccueil = () => {
                 </div>
             <div className="contenu-panneau-acc">
                 <h1 className="titre">
-                    <span className="prenom">Thibault</span>
-                    <span className="nom">Schmitt</span>
+                    <span className="prenom">{textAffichayA}</span>
+                    <span className="nom">{textAffichayB}</span>
                 </h1>
                 <h3 className="sous-titre">
-                   <span className="dev">Développeur</span> 
-                   <span className="web">web</span>
+                   <span className="dev">{textAffichayC}</span> 
+                   <span className="web">{textAffichayD}</span>
                 </h3>
                 <div className="pres-site">
-                    <span>Bienvenue sur mon site web ! J'espère que l'expérience vous plaira.</span><br/>
-                    <span>Il est tout nouveau, je travaille dessus en ce moment.</span> </div>
+                    <span>{textAffichayE}</span>
+                </div>
             </div>
         </div>
     )
--- src/donnees/constantesCompetence.js
+++ src/donnees/constantesCompetence.js
@@ -28,6 +28,7 @@ module.exports = {
           "Git",
           "Docker",
           "Less",
+          "VBA"
         ],
         missions: [
           {

5ce5d944dec462a730c78939f02dd5a2f81f8399
thibDev <schmitt.thibault34070@gmail.com>
2024-04-02 12:34:23 -0400

    Correction bogue et correction orthographe.

--- src/components/accueil/PanneauAcceuil.js
+++ src/components/accueil/PanneauAcceuil.js
@@ -7,11 +7,13 @@ const TEXTE = require("../../donnees/acc/textAcc");
 const PanneauAccueil = () => {
 
     const lang = useContext(LangContext);
-   const [textAffichayA, settextAffichayA] = useState("");
-   const [textAffichayB, settextAffichayB] = useState("");
-   const [textAffichayC, settextAffichayC] = useState("");
-   const [textAffichayD, settextAffichayD] = useState("");
-   const [textAffichayE, settextAffichayE] = useState("");
+    
+    const [ancienneLang, setAncienneLang] = useState(lang)
+    const [textAffichayA, settextAffichayA] = useState("");
+    const [textAffichayB, settextAffichayB] = useState("");
+    const [textAffichayC, settextAffichayC] = useState("");
+    const [textAffichayD, settextAffichayD] = useState("");
+    const [textAffichayE, settextAffichayE] = useState("");
 
     const texteA = TEXTE[lang].texte1;
     const texteB = TEXTE[lang].texte2;
@@ -19,56 +21,55 @@ const PanneauAccueil = () => {
     const texteD = TEXTE[lang].texte4;
     const texteE = TEXTE[lang].texte5;
 
-    const changementLange = () => {
-        settextAffichayA("");
-        settextAffichayB("");
-        settextAffichayC("");
-        settextAffichayD("");
-        settextAffichayE("");
-    }
-    
-    useEffect(()=>{
-        if(textAffichayA.length < texteA.length){
-            setTimeout(()=>{
+    //On test si l'ancienne langue et la nouvelle son les mêmes pour pas avoir de bogue lors du changement de langue
+    //dans le cas ou l'animation n'est pas fini.
+    useEffect(() => {
+        if(lang === ancienneLang){
+        if (textAffichayA.length < texteA.length) {
+            setTimeout(() => {
                 settextAffichayA(textAffichayA + texteA[textAffichayA.length]);
             }, 150)
-        } else if(textAffichayB.length < texteB.length){
-            setTimeout(()=>{
+        } else if (textAffichayB.length < texteB.length) {
+            setTimeout(() => {
                 settextAffichayB(textAffichayB + texteB[textAffichayB.length]);
             }, 150)
-        } else if(textAffichayC.length < texteC.length){
-            setTimeout(()=>{
+        } else if (textAffichayC.length < texteC.length) {
+            setTimeout(() => {
                 settextAffichayC(textAffichayC + texteC[textAffichayC.length]);
             }, 150)
-        }else if(textAffichayD.length < texteD.length){
-            setTimeout(()=>{
+        } else if (textAffichayD.length < texteD.length) {
+            setTimeout(() => {
                 settextAffichayD(textAffichayD + texteD[textAffichayD.length]);
             }, 150)
-        }else if(textAffichayE.length < texteE.length){
-            setTimeout(()=>{
+        } else if (textAffichayE.length < texteE.length) {
+            setTimeout(() => {
                 settextAffichayE(textAffichayE + texteE[textAffichayE.length]);
-            }, 70)
+            }, 20)
+        }} else {
+            settextAffichayA("");
+            settextAffichayB("");
+            settextAffichayC("");
+            settextAffichayD("");
+            settextAffichayE("");
+            setAncienneLang(lang)
         }
     }, [textAffichayA, textAffichayB, textAffichayC, textAffichayD, textAffichayE])
 
-    useEffect(()=>{
-        changementLange();
-    }, [lang])
 
     return (
         <div className="panneau-acc">
 
-                <div className="fond-ecran">
-                    <img src={require("../../img/acc/fondreact.png")}/>
-                </div>
+            <div className="fond-ecran">
+                <img src={require("../../img/acc/fondreact.png")} />
+            </div>
             <div className="contenu-panneau-acc">
                 <h1 className="titre">
                     <span className="prenom">{textAffichayA}</span>
                     <span className="nom">{textAffichayB}</span>
                 </h1>
                 <h3 className="sous-titre">
-                   <span className="dev">{textAffichayC}</span> 
-                   <span className="web">{textAffichayD}</span>
+                    <span className="dev">{textAffichayC}</span>
+                    <span className="web">{textAffichayD}</span>
                 </h3>
                 <div className="pres-site">
                     <span>{textAffichayE}</span>

d39619e0e8df21c78cf1ca65d9857fbd816f6190
thibDev <schmitt.thibault34070@gmail.com>
2024-04-02 13:01:31 -0400

    Changement texte biere pong.

--- src/donnees/projet/texteProjet.js
+++ src/donnees/projet/texteProjet.js
@@ -7,7 +7,7 @@ module.exports = {
         projets:[
             {nom:"Antidote",  description: "Correcteur orthographique. Je travaillais en tant que frontend sur l'application.", lien: "https://www.antidote.info/en/antidote-web", projetPro: true, className: "antidote"},
             {nom:"Portfolio Junior", description: "Mon premier portfolio en Vuejs.", lien: "https://schmittthibault.com/#/",  projetPro: false, className: "junior"},
-            {nom:"Classement bière pong", description: "Création d'application pour m'entrainer avec NodeJs.", lien: "https://bestofbeerpong.com/", projetPro: false, className: "beerpong"},
+            {nom:"Classement bière pong", description: "Classement entre amis. Création d'application pour m'entrainer avec NodeJs.", lien: "https://bestofbeerpong.com/", projetPro: false, className: "beerpong"},
             {nom:"Git hub", description: "Mon Git Hub. vous pourrez y retrouver mes autres projets.", lien: "https://github.com/TiboLeFreapon/", projetPro: false, className:"github"},
         ]
     },
@@ -19,7 +19,7 @@ module.exports = {
         projets:[
             {nom:"Antidote", description: "Spell checker. I was working as a frontend on the application.", lien: "https://www.antidote.info/en/antidote-web", projetPro: true,  className: "antidote"},
             {nom:"Junior Portfolio", description: "My first portfolio in Vuejs.", lien: "https://schmittthibault.com/#/", projetPro: false,  className: "junior"},
-            {nom:"Beer pong ranking", description: "My friends and I wanted to know what was best at beerpong. So I created a ranking with statistics.", lien: "https://bestofbeerpong.com/", projetPro: false, className: "beerpong"},
+            {nom:"Beer pong ranking", description: "Ranking among friends. Application creation to train with NodeJs.", lien: "https://bestofbeerpong.com/", projetPro: false, className: "beerpong"},
             {nom:"Git hub", description: "My Git Hub. You'll find my other projects.", lien: "https://github.com/TiboLeFreapon/", projetPro: false, className:"github"},
         ]
     }
